2e79d2756789e7111aef4fe8f900fc13c04f05c2
Girish <m.girishroy@gmail.com>
2023-03-07 22:35:13 +0530

    basic server setup

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,21 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 
-const connectDB = (url) => {
-  return connect(url);
+const connectDB = async () => {
+  // mongoose.set("useCreateIndex", true);
+  mongoose.set("strictQuery", false);
+  try {
+    const URL = process.env.MONGO_URL;
+    const conn = await mongoose.connect(URL, {
+      dbName: "E_COMMERCE",
+      autoIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB", conn.connection.host);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
